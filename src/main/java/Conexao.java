2ca1871dbc4b8e01a46be696a89602c8252eabba
domen1c0 <paolodomenico777@gmail.com>
2022-03-17 18:11:14 -0300

    commit inicial

---
+++ src/main/java/Conexao.java
@@ -0,0 +1,12 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Conexao {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Usuario> usuarios = new ArrayList();
+
+    }
+}
